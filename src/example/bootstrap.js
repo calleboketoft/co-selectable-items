2f35d9d4fac99b34d7d211cd7ff6b313b26e8ce3
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-11-04 11:02:27 +0100

    wat

--- src/example/bootstrap.js
+++
@@ -1,6 +0,0 @@
-require('zone.js');
-require('reflect-metadata');
-var angular2_1 = require('angular2/angular2');
-var app_cmp_1 = require('./app-cmp');
-angular2_1.bootstrap(app_cmp_1.AppCmp);
-//# sourceMappingURL=bootstrap.js.map
