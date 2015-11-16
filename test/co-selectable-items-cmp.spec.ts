7c5db7654d798093c5424b30149e43a941288a9b
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-11-16 20:46:01 +0100

    prepared for tests

---
+++ test/co-selectable-items-cmp.spec.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata'
+import 'zone.js'
+import { CoSelectableItemsCmp } from '../src/co-selectable-items/co-selectable-items-cmp'
+
+describe('CoSelectableItemsCmp', function () {
+  it('Should be defined', function () {
+    expect(CoSelectableItemsCmp).toBeDefined()
+  })
+})
