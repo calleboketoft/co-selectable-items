adfc66657b4f3d291959e07ecd6bf87e92fb8fd5
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-11-19 16:19:48 +0100

    code formatting

--- src/co-selectable-items/co-selectable-items-cmp.ts
+++ src/co-selectable-items/co-selectable-items-cmp.ts
@@ -204,22 +204,25 @@ export class CoSelectableItemsCmp {
 // http://stackoverflow.com/questions/25456013/javascript-deepequal-comparison
 function deepEqual(x, y) {
   if ((typeof x == 'object' && x != null) && (typeof y == 'object' && y != null)) {
-    if (Object.keys(x).length != Object.keys(y).length)
+    if (Object.keys(x).length != Object.keys(y).length) {
       return false
-
+    }
     for (var prop in x) {
       if (y.hasOwnProperty(prop)) {
-        if (!deepEqual(x[prop], y[prop]))
+        if (!deepEqual(x[prop], y[prop])) {
           return false
+        }
       }
-      else
+      else {
         return false
+      }
     }
-
     return true
   }
-  else if (x !== y)
+  else if (x !== y) {
     return false
-  else
+  }
+  else {
     return true
+  }
 }
