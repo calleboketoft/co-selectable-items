9fd310b47dc8e1b3109144fc8e33590f5ba539c6
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-05-31 14:59:54 +0200

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -11,24 +11,24 @@ gulp.task('sass', function () {
 // E2E tests with Protractor
 // Ironically, the instructions for how to get this up and running was found at
 // the site for gulp-protractor plugin: https://github.com/mllrsohn/gulp-protractor
-// var path = require('path')
-// var child_process = require('child_process')
-// var express = require('express')
-// var http = require('http')
-// var server = http.createServer(express().use(express.static(__dirname)))
+var path = require('path')
+var child_process = require('child_process')
+var express = require('express')
+var http = require('http')
+var server = http.createServer(express().use(express.static(__dirname)))
 
-// gulp.task('protractor-run', ['serve-files'], (done) => {
-//   var argv = process.argv.slice(3) // forward args to protractor
-//   child_process.spawn(getProtractorBinary('protractor'), argv, {
-//     stdio: 'inherit'
-//   }).once('close', done)
-// })
-// gulp.task('serve-files', (done) => server.listen(3000, done))
-// gulp.task('test-e2e', ['protractor-run'], (done) => server.close())
+gulp.task('protractor-run', ['serve-files'], (done) => {
+  var argv = process.argv.slice(3) // forward args to protractor
+  child_process.spawn(getProtractorBinary('protractor'), argv, {
+    stdio: 'inherit'
+  }).once('close', done)
+})
+gulp.task('serve-files', (done) => server.listen(3000, done))
+gulp.task('test-e2e', ['protractor-run'], (done) => server.close())
 
-// function getProtractorBinary(binaryName){
-//   var winExt = /^win/.test(process.platform)? '.cmd' : ''
-//   var pkgPath = require.resolve('protractor')
-//   var protractorDir = path.resolve(path.join(path.dirname(pkgPath), '..', 'bin'))
-//   return path.join(protractorDir, '/' + binaryName + winExt)
-// }
+function getProtractorBinary(binaryName){
+  var winExt = /^win/.test(process.platform)? '.cmd' : ''
+  var pkgPath = require.resolve('protractor')
+  var protractorDir = path.resolve(path.join(path.dirname(pkgPath), '..', 'bin'))
+  return path.join(protractorDir, '/' + binaryName + winExt)
+}
